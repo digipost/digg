5ac6fb6e4868d8e43c221df46a9bd1e77664753f
Rune Flobakk <flobakk@gmail.com>
2015-09-07 00:55:15 +0200

    SimpleIterator. Based on implementation from Motif, adapted for Java 8.

---
+++ src/main/java/no/digipost/collection/SimpleIterator.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.collection;
+
+import no.digipost.exceptions.Exceptions;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+/**
+ * SimpleIterator offers an easier way to implement the
+ * {@link java.lang.Iterator} interface, by requiring only
+ * one method, {@link #nextIfAvailable()}, to be implemented instead of both
+ * {@link Iterator#hasNext() hasNext()} and {@link Iterator#next() next()}.
+ *
+ * In addition, implementations are free to throw any exception, which,
+ * if is a checked exception, will be rethrown wrapped in a <code>RuntimeException</code>.
+ *
+ * @param <T> The type of elements yielded by this iterator.
+ * @see #nextIfAvailable()
+ */
+public abstract class SimpleIterator<T> implements Iterator<T> {
+
+    private Optional<? extends T> next = Optional.empty();
+
+    /**
+     * @return The next element if any, or {@link Optional#empty() empty} if there are no more elements.
+     */
+    protected abstract Optional<? extends T> nextIfAvailable() throws Exception;
+
+    @Override
+    public final boolean hasNext() {
+        try {
+            next = nextIfAvailable();
+        } catch (Exception e) {
+            throw Exceptions.asUnchecked(e);
+        }
+        return next.isPresent();
+    }
+
+    @Override
+    public final T next() {
+        if (!next.isPresent() && !hasNext()) throw new NoSuchElementException();
+        T toReturn = next.get();
+        next = Optional.empty();
+        return toReturn;
+    }
+
+    @Override
+    public void remove() {
+    	throw new UnsupportedOperationException("remove() method is not supported");
+    }
+
+}
---
+++ src/test/java/no/digipost/collection/SimpleIteratorTest.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.collection;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class SimpleIteratorTest {
+
+	@Rule
+	public final ExpectedException expectedException = ExpectedException.none();
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void nextThrowsNoSuchElement() {
+        empty.iterator().next();
+    }
+
+    @Test
+    public void nextReturnsAnElementThenThrowsNoSuchElement() {
+        Iterator<String> iterator = oneElement.iterator();
+        assertThat(iterator.next(), is("x"));
+        try { iterator.next(); } catch (NoSuchElementException e) { return; }
+        fail("Should have thrown " + NoSuchElementException.class);
+    }
+
+    @Test
+    public void iteratingUsingForLoop() {
+        for (Object o : empty) fail("Should not yield any object, but got " + o);
+        for (String s : oneElement) {
+            assertThat(s, is("x"));
+            return;
+        }
+        fail("Did not iterate");
+    }
+
+
+    @Test
+    public void iteratorMayThrowException() {
+        Iterator<Object> iterator = fails.iterator();
+        try {
+            iterator.next();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(RuntimeException.class));
+            assertThat(e.getCause(), instanceOf(IOException.class));
+            return;
+        }
+        fail("Should throw exception");
+    }
+
+	@Test
+	public void removeIsUnsupported() {
+		Iterator<String> iterator = oneElement.iterator();
+		iterator.next();
+		expectedException.expect(UnsupportedOperationException.class);
+		iterator.remove();
+	}
+
+
+
+
+
+
+    private final Iterable<String> oneElement = new Iterable<String>() {
+        @Override
+        public Iterator<String> iterator() {
+            return new SimpleIterator<String>() {
+                boolean returned;
+                @Override
+                protected Optional<String> nextIfAvailable() {
+                    if (returned) return Optional.empty();
+                    returned = true;
+                    return Optional.of("x");
+                }
+            };
+        }
+    };
+
+
+    private final Iterable<Object> empty = new Iterable<Object>() {
+        @Override
+        public Iterator<Object> iterator() {
+            return new SimpleIterator<Object>() {
+                @Override
+                protected Optional<Object> nextIfAvailable() {
+                    return Optional.empty();
+                }
+            };
+        }
+    };
+
+    private final Iterable<Object> fails = new Iterable<Object>() {
+        @Override
+        public Iterator<Object> iterator() {
+            return new SimpleIterator<Object>() {
+                @Override
+                protected Optional<? extends Object> nextIfAvailable() throws Exception {
+                    throw new IOException();
+                }
+            };
+        }
+    };
+
+}
