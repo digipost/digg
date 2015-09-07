b536f2d14fe4e107328729e4306ca79a7e392bbd
Rune Flobakk <flobakk@gmail.com>
2015-09-07 10:39:44 +0200

    Compare-utils. min/max-methods.

---
+++ src/main/java/no/digipost/Compare.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost;
+
+public final class Compare {
+
+	public static <T extends Comparable<T>> T min(T t1, T t2) {
+		return t1.compareTo(t2) > 0 ? t2 : t1;
+	}
+
+	public static <T extends Comparable<T>> T max(T t1, T t2) {
+		return t2.compareTo(t1) > 0 ? t2 : t1;
+	}
+
+	private Compare() {}
+}
---
+++ src/test/java/no/digipost/CompareTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static no.digipost.Compare.max;
+import static no.digipost.Compare.min;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+
+@RunWith(Theories.class)
+public class CompareTest {
+
+	@Theory
+	public void minAndMax(@ForAll(sampleSize=10) int x, @ForAll(sampleSize=10) int y) {
+		assumeThat(x, not(y));
+		assertThat(min(x, y), not(max(x, y)));
+		assertThat(min(x, y), is(min(y, x)));
+		assertThat(max(x, y), is(max(y, x)));
+	}
+}
