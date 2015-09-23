48d7beb431eb0f3977f9f74014f126b934957aa7
Rune Flobakk <flobakk@gmail.com>
2015-09-23 22:46:52 +0200

    Fix compiler warning "unchecked generic array..."
    "unchecked generic array creation for varargs parameter"

--- src/test/java/no/digipost/collection/BatchedIterableTest.java
+++ src/test/java/no/digipost/collection/BatchedIterableTest.java
@@ -33,55 +33,59 @@ import static org.junit.Assert.assertThat;
 public class BatchedIterableTest {
 
 
-	@Test
-	public void emptyBatch() {
-		Batches<Object> empty1 = new Batches<>();
-		assertThat(batched(empty1, b -> false), is(emptyIterable()));
-		assertThat(empty1.fetches(), is(1));
+    @Test
+    public void emptyBatch() {
+        Batches<Object> empty1 = new Batches<>();
+        assertThat(batched(empty1, b -> false), is(emptyIterable()));
+        assertThat(empty1.fetches(), is(1));
 
-		Batches<Object> empty2 = new Batches<>();
-		assertThat(batched(empty2, b -> true), is(emptyIterable()));
-		assertThat(empty2.fetches(), is(2));
-	}
+        Batches<Object> empty2 = new Batches<>();
+        assertThat(batched(empty2, b -> true), is(emptyIterable()));
+        assertThat(empty2.fetches(), is(2));
+    }
 
-	@Test
-	public void singleBatch() {
-		Batches<String> singleBatch = new Batches<String>(asList("a", "b"));
-		assertThat(batched(singleBatch, b -> false), contains("a", "b"));
-		assertThat(singleBatch.fetches(), is(1));
-	}
+    @Test
+    public void singleBatch() {
+        Batches<String> singleBatch = new Batches<String>(asList("a", "b"));
+        assertThat(batched(singleBatch, b -> false), contains("a", "b"));
+        assertThat(singleBatch.fetches(), is(1));
+    }
 
-	@Test
-	public void twoBatches() {
-		Batches<String> twoBatches = new Batches<String>(asList("a", "b", "c"), asList("d", "e"));
-		assertThat(batched(twoBatches, batch -> batch.size() >= 3), contains("a", "b", "c", "d", "e"));
-		assertThat(twoBatches.fetches(), is(2));
-	}
+    @Test
+    public void twoBatches() {
+        Batches<String> twoBatches = new Batches<String>(asList("a", "b", "c"), asList("d", "e"));
+        assertThat(batched(twoBatches, batch -> batch.size() >= 3), contains("a", "b", "c", "d", "e"));
+        assertThat(twoBatches.fetches(), is(2));
+    }
 
 
 
-	class Batches<T> implements Supplier<List<T>> {
+    static class Batches<T> implements Supplier<List<T>> {
 
-		private final Queue<List<T>> batches;
-		private int fetches;
+        private final Queue<List<T>> batches;
+        private int fetches;
 
-		@SafeVarargs
+        @SuppressWarnings("unchecked")
+        Batches() {
+            this(new List[0]);
+        }
+        @SafeVarargs
         Batches(List<T> ... batches) {
-			this.batches = new LinkedList<>();
-			for (List<T> batch : batches) {
-				this.batches.add(batch);
-			}
-		}
-
-		@Override
-        public List<T> get() {
-			fetches++;
-			return Optional.ofNullable(batches.poll()).orElse(emptyList());
+            this.batches = new LinkedList<>();
+            for (List<T> batch : batches) {
+                this.batches.add(batch);
+            }
         }
 
-		public int fetches() {
-			return fetches;
-		}
-	}
+        @Override
+        public List<T> get() {
+            fetches++;
+            return Optional.ofNullable(batches.poll()).orElse(emptyList());
+        }
+
+        public int fetches() {
+            return fetches;
+        }
+    }
 
 }
