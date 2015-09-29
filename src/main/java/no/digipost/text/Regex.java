21277e3d48a41448c5931c69af04fbe3dd41afc5
Rune Flobakk <flobakk@gmail.com>
2015-09-29 18:04:23 +0200

    Regex group extraction with Stream API.

---
+++ src/main/java/no/digipost/text/Regex.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.text;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public final class Regex {
+
+    /**
+     * Extract the groups from text which matches the given pattern.
+     *
+     * @param text the text to extract from
+     * @param pattern The pattern which contains groups to extract.
+     *
+     * @return The extracted groups. If the pattern does not match or does not define
+     *         any groups, the empty {@link Stream#empty() stream} is returned.
+     */
+    public static Stream<String> extractGroups(CharSequence text, Pattern pattern) {
+        return Optional.of(text).map(pattern::matcher).map(Regex::groups).orElseGet(Stream::empty);
+    }
+
+
+    /**
+     * Extract the groups from a {@code Matcher}, if a match is {@link Matcher#find() found}.
+     *
+     * @param matcher the {@code Matcher}.
+     *
+     * @return The extracted groups, or the empty {@link Stream#empty() stream} if no match is
+     *         found or no groups are defined.
+     */
+    public static Stream<String> groups(Matcher matcher) {
+        if (matcher.find()) {
+            return Stream.iterate(1, i -> i + 1).limit(matcher.groupCount()).map(matcher::group);
+        } else {
+            return Stream.empty();
+        }
+    }
+
+
+    private Regex() {}
+}
---
+++ src/test/java/no/digipost/text/RegexTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.text;
+
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static co.unruly.matchers.StreamMatchers.contains;
+import static co.unruly.matchers.StreamMatchers.empty;
+import static org.junit.Assert.assertThat;
+
+public class RegexTest {
+
+    @Test
+    public void extractGroups() {
+        Pattern pattern = Pattern.compile("ab(\\d\\d)cd(\\d)");
+        assertThat(Regex.extractGroups("ab12cd3", pattern), contains("12", "3"));
+    }
+
+    @Test
+    public void noGroupsYieldsEmptyStream() {
+        Pattern pattern = Pattern.compile(".*");
+        assertThat(Regex.extractGroups("abc", pattern), empty());
+    }
+
+    @Test
+    public void noMatchYieldsEmptyStream() {
+        Pattern pattern = Pattern.compile("(a)");
+        assertThat(Regex.extractGroups("b", pattern), empty());
+    }
+}
