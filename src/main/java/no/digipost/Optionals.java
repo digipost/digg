26745e70e9d03542ab38714222d6c2836a8c0126
Rune Flobakk <flobakk@gmail.com>
2016-01-06 09:55:46 +0100

    Javadoc. Returned list from Optional.toList is capacity-optimized (1).

--- src/main/java/no/digipost/Optionals.java
+++ src/main/java/no/digipost/Optionals.java
@@ -15,17 +15,45 @@
  */
 package no.digipost;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toCollection;
+
+/**
+ * Utilities for working with {@link Optional}s.
+ */
 public final class Optionals {
 
+    /**
+     * Unwrap an {@link Optional} into a zero or one element {@link Collections#unmodifiableList(List) unmodifiable} list.
+     *
+     * @param <T> The type of the optional value.
+     * @param optional The optional value to be wrapped in a list.
+     *
+     * @return list containing either exactly one or zero elements depending of the
+     *         {@code Optional} being present or not.
+     */
     public static <T> List<T> toList(Optional<T> optional) {
-        return collect(optional, Collectors.toList());
+        return unmodifiableList(collect(optional, toCollection(() -> new ArrayList<>(1))));
     }
 
+    /**
+     * Unwrap an {@link Optional} into another container using a {@link Collector}.
+     *
+     * @param <T> The type of the optional value.
+     * @param <R> The resulting container type.
+     *
+     * @param optional The optional value to be wrapped in another container.
+     * @param collector The collector to use for wrapping into another container.
+     *
+     * @return The new container with either exactly one or zero elements depending of the
+     *         {@code Optional} being present or not.
+     */
     public static <T, A, R> R collect(Optional<T> optional, Collector<T, A, R> collector) {
         A container = collector.supplier().get();
         optional.ifPresent(o -> collector.accumulator().accept(container, o));
--- src/test/java/no/digipost/OptionalsTest.java
+++ src/test/java/no/digipost/OptionalsTest.java
@@ -15,8 +15,11 @@
  */
 package no.digipost;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.util.List;
 import java.util.Optional;
 
 import static no.digipost.Optionals.toList;
@@ -26,9 +29,28 @@ import static org.junit.Assert.assertThat;
 
 public class OptionalsTest {
 
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
     @Test
     public void convertOptionalToList() {
         assertThat(toList(Optional.of(42)), contains(42));
         assertThat(toList(Optional.empty()), empty());
+    }
+
+    @Test
+    public void cannotAddToReturnedList() {
+        List<Integer> list = toList(Optional.of(42));
+
+        expectedException.expect(UnsupportedOperationException.class);
+        list.add(43);
+    }
+
+    @Test
+    public void cannotRemoveFromReturnedList() {
+        List<Integer> list = toList(Optional.of(42));
+
+        expectedException.expect(UnsupportedOperationException.class);
+        list.clear();
     }
 }
