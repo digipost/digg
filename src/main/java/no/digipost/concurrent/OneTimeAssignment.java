44e1c9ec4f7b12cf994a0aa0d43d864f1774762d
Rune Flobakk <flobakk@gmail.com>
2016-01-19 15:38:26 +0100

    OneTimeAssignment: a reference which may only be assigned once.

---
+++ src/main/java/no/digipost/concurrent/OneTimeAssignment.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.concurrent;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+
+/**
+ * A reference which may or may not be assigned a value, with the added constraint
+ * that it can only be assigned once. If it is assigned a value more than one time
+ * it will throw an exception.
+ * <p>
+ * The class is thread-safe in the sense that it is not possible for concurrent threads to
+ * assign a value to it twice, though relevant cases for concurrent use are probably limited. The
+ * motivation is rather to enable fail-fast for erroneous API-usage involving assignments,
+ * e.g. builder implementations.
+ *
+ *
+ * @param <V> The type of the object which is referenced.
+ */
+public final class OneTimeAssignment<V> {
+
+    /**
+     * Create a new non-assigned instance with a default value.
+     *
+     * @param defaultValue The default value to use in case an assignment
+     *                     through {@link #set(Object)} is never performed.
+     * @see #defaultTo(Supplier)
+     */
+    public static final <V> OneTimeAssignment<V> defaultTo(V defaultValue) {
+        return defaultTo(() -> defaultValue);
+    }
+
+    /**
+     * Create a new non-assigned instance with a default value.
+     *
+     * @param defaultValue The supplier to acquire the default value to use in case an
+     *                     assignment through {@link #set(Object)} is never performed.
+     */
+    public static final <V> OneTimeAssignment<V> defaultTo(Supplier<? extends V> defaultValue) {
+        return new OneTimeAssignment<>(defaultValue);
+    }
+
+    /**
+     * Create a new non-assigned instance.
+     */
+    public static final <V> OneTimeAssignment<V> newInstance() {
+        return new OneTimeAssignment<>(() -> null);
+    }
+
+
+
+    private final AtomicReference<V> ref = new AtomicReference<>();
+    private final Supplier<? extends V> defaultValue;
+
+    private OneTimeAssignment(Supplier<? extends V> defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    /**
+     * Assigns a value to this reference. This method can only be called once.
+     *
+     * @param value the value to set.
+     * @throws AlreadyAssigned if this reference is already assigned a value.
+     */
+    public void set(V value) {
+        if (!ref.compareAndSet(null, value)) {
+            throw new AlreadyAssigned(ref.get(), value);
+        }
+    }
+
+    /**
+     * @return the referenced value. If the reference has not yet been set but is
+     *         {@link #defaultTo(Supplier) initialized with a default value},
+     *         the reference is assigned the default value and this is returned.
+     */
+    public V get() {
+        return ref.updateAndGet(v -> v != null ? v : defaultValue.get());
+    }
+
+
+    public static final class AlreadyAssigned extends IllegalStateException {
+        private AlreadyAssigned(Object alreadyAssignedValue, Object attemptedAssignedValue) {
+            super("Already assigned to " + alreadyAssignedValue + ". Can not be reassigned to " + attemptedAssignedValue);
+        }
+    }
+
+}
---
+++ src/test/java/no/digipost/concurrent/OneTimeAssignmentTest.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.concurrent;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class OneTimeAssignmentTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Rule
+    public final Timeout timeout = new Timeout(2, SECONDS);
+
+    @Test
+    public void assignAndRetrieveAValue() {
+        OneTimeAssignment<String> x = OneTimeAssignment.newInstance();
+        assertThat(x.get(), nullValue());
+        assertThat(x.get(), nullValue());
+        x.set("x");
+        assertThat(x.get(), is("x"));
+        assertThat(x.get(), is("x"));
+    }
+
+    @Test
+    public void useDefaultValueIfNoValueIsAssigned() {
+        OneTimeAssignment<String> y = OneTimeAssignment.defaultTo("y");
+        assertThat(y.get(), is("y"));
+    }
+
+    @Test
+    public void settingAValueDiscardsDefaultValue() {
+        OneTimeAssignment<String> x = OneTimeAssignment.defaultTo("y");
+        x.set("x");
+        assertThat(x.get(), is("x"));
+    }
+
+    @Test
+    public void setValueTwiceThrowsException() {
+        OneTimeAssignment<String> x = OneTimeAssignment.newInstance();
+        x.set("x");
+        expectedException.expect(OneTimeAssignment.AlreadyAssigned.class);
+        x.set("x");
+    }
+
+    @Test
+    public void keepsTheFirstAssignedValueOnErroneousDoubleAssignment() {
+        OneTimeAssignment<String> x = OneTimeAssignment.defaultTo("y");
+        x.get();
+        try {
+            x.set("x");
+            fail("should throw exception");
+        } catch (OneTimeAssignment.AlreadyAssigned e) {
+            assertThat(x.get(), is("y"));
+        }
+    }
+
+    @Test
+    public void onlyPossibleToConcurrentlyAssignOnce() throws InterruptedException {
+        int concurrentAssignments = 500;
+        CountDownLatch expectedFails = new CountDownLatch(concurrentAssignments - 1);
+        OneTimeAssignment<String> x = OneTimeAssignment.defaultTo("y");
+        Stream<Runnable> assignments = Stream.iterate(() -> {
+            try {
+                x.set("x");
+                return;
+            } catch (OneTimeAssignment.AlreadyAssigned e) {
+                expectedFails.countDown();
+            }
+        }, (Runnable r) -> r);
+
+        assignments.limit(concurrentAssignments).parallel().forEach(CompletableFuture::runAsync);
+        expectedFails.await();
+        assertThat(x.get(), is("x"));
+    }
+}
