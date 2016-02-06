3ec0e87e8df00bc80ee6486ca3d5c72d2f84dc15
Rune Flobakk <flobakk@gmail.com>
2016-02-06 18:44:42 +0100

    Add some javadocs to DiggCollectors

--- src/main/java/no/digipost/DiggCollectors.java
+++ src/main/java/no/digipost/DiggCollectors.java
@@ -25,19 +25,26 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Collections.unmodifiableList;
 
+/**
+ * Various {@link java.util.stream.Collector} implementations.
+ */
 public final class DiggCollectors {
 
-    public static <T, A, R> AdaptableCollector<T, A, R> adapt(Collector<T, A, R> collector) {
-        if (collector instanceof AdaptableCollector) {
-            return (AdaptableCollector<T, A, R>) collector;
-        } else {
-            return AdaptableCollector.of(collector.supplier(), collector.accumulator(), collector.combiner(), collector.finisher(), collector.characteristics());
-        }
-    }
-
+    /**
+     * A multimap maps from keys to lists, and this collector will arrange {@link ViewableAsTuple tuples}
+     * by putting each distinct {@link Tuple#first() first tuple-element} as keys of the resulting map, mapping
+     * them to a {@link List}, and adding each {@link Tuple#second() second tuple-element} to the list.
+     *
+     * @param <T1> The type of the first tuple element, which will become the key type of the resulting {@code Map}.
+     * @param <T2> The type of the second tuple element, which will become the {@code List<T2>} value type of the
+     *             resulting {@code Map}.
+     *
+     * @return the multimap collector.
+     */
     public static <T1, T2> AdaptableCollector<ViewableAsTuple<T1, Optional<T2>>, ?, Map<T1, List<T2>>> toMultimap() {
         Function<ViewableAsTuple<T1, Optional<T2>>, Tuple<T1, Optional<T2>>> asTuple = ViewableAsTuple::asTuple;
         return toMultimap(asTuple.andThen(Tuple::first), asTuple.andThen(Tuple::second));
@@ -52,6 +59,39 @@ public final class DiggCollectors {
     }
 
 
+    /**
+     * This is a collector for accessing the <em>expected only</em> element of a {@link Stream}, as
+     * it will throw an exception if more than one element is processed. This should be used in
+     * preference of the {@link Stream#findFirst()} or {@link Stream#findAny()} when it is imperative
+     * that the stream indeed yields a maximum of one single element, and any more elements is
+     * considered a programming error.
+     *
+     * @return the collector
+     */
+    public static <T> AdaptableCollector<T, OneTimeAssignment<T>, Optional<T>> allowAtMostOne() {
+        return adapt(new EnforceAtMostOneElementCollector<>());
+    }
+
+
+    /**
+     * Turn any {@link Collector} into an {@link AdaptableCollector}.
+     *
+     * @param collector the collector to convert
+     *
+     * @return the {@link AdaptableCollector}
+     *
+     * @see AdaptableCollector
+     */
+    public static <T, A, R> AdaptableCollector<T, A, R> adapt(Collector<T, A, R> collector) {
+        if (collector instanceof AdaptableCollector) {
+            return (AdaptableCollector<T, A, R>) collector;
+        } else {
+            return AdaptableCollector.of(collector.supplier(), collector.accumulator(), collector.combiner(), collector.finisher(), collector.characteristics());
+        }
+    }
+
+
+
     private static <T> List<T> concat(Collection<? extends T> list1, Collection<? extends T> list2) {
         List<T> newList = new ArrayList<>(list1.size() + list2.size());
         newList.addAll(list1);
@@ -59,10 +99,6 @@ public final class DiggCollectors {
         return unmodifiableList(newList);
     }
 
-    public static <T> AdaptableCollector<T, OneTimeAssignment<T>, Optional<T>> allowAtMostOne() {
-        return adapt(new EnforceAtMostOneElementCollector<>());
-    }
-
-    public DiggCollectors() {}
+    private DiggCollectors() {}
 
 }
