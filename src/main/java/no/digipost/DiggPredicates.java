6018be8e466f2415806210d2459aee48fc3f5e5a
Rune Flobakk <flobakk@gmail.com>
2016-05-20 00:58:17 +0200

    Add DiggPredicates.nth(..)
    The (stateful) predicate will yield true for the nth `true`-yielding invocation of
    a wrapped predicate. Can be used to find the nth matching element in a
    Stream.

---
+++ src/main/java/no/digipost/DiggPredicates.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
+
+public final class DiggPredicates {
+
+    /**
+     * Create a new predicate which only yields true on the <em>nth</em>
+     * {@code true}-yielding invocation of a given predicate.
+     *
+     * <p>
+     * As this is a stateful predicate, it should usually not be kept as a
+     * reference, but used inline when processing a number of elements,
+     * with {@link java.util.Stream#filter streams}.
+     *
+     * @param n the nth {@code true}-yielding invocation
+     * @param predicate the predicate
+     * @return the new predicate
+     */
+    public static <T> Predicate<T> nth(long n, Predicate<T> predicate) {
+        AtomicLong foundMatches = new AtomicLong();
+        return t -> predicate.test(t) && foundMatches.incrementAndGet() == n;
+    }
+
+    private DiggPredicates() {}
+}
---
+++ src/test/java/no/digipost/DiggPredicatesTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static no.digipost.DiggPredicates.nth;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitQuickcheck.class)
+public class DiggPredicatesTest {
+
+    @Property
+    public void yieldsTrueOnGivenInvocationNumber(@InRange(minInt=0, maxInt=1024) int invocationNum) {
+        Predicate<Integer> isNthEvenNumber = nth(invocationNum, t -> t % 2 == 0);
+        Integer nthEvenNumber = Stream.iterate(1, i -> i + 1).limit(invocationNum * 2).filter(isNthEvenNumber).findFirst().get();
+        assertThat(nthEvenNumber, is(invocationNum * 2));
+    }
+
+}
