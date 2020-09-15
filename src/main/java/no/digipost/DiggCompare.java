ae0d1f29fb5ffd90549f8600a8729b194f92f7b3
Rune Flobakk <flobakk@gmail.com>
2020-09-16 00:51:49 +0200

    Parameterize choosing min and max

--- src/main/java/no/digipost/DiggCompare.java
+++ src/main/java/no/digipost/DiggCompare.java
@@ -15,14 +15,115 @@
  */
 package no.digipost;
 
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+import static java.util.Comparator.comparing;
+
+
+/**
+ * Utilities for comparing values.
+ */
 public final class DiggCompare {
 
+    /**
+     * Choose the lesser of two {@link Comparable} objects.
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * @return the least of the two given objects.
+     */
     public static <T extends Comparable<T>> T min(T t1, T t2) {
         return t1.compareTo(t2) > 0 ? t2 : t1;
     }
 
+    /**
+     * Choose the greater of two {@link Comparable} objects.
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * @return the greatest of the two given objects.
+     */
     public static <T extends Comparable<T>> T max(T t1, T t2) {
         return t2.compareTo(t1) > 0 ? t2 : t1;
+    }
+
+    /**
+     * Choose the lesser of two objects by using a given {@link Comparator}.
+     *
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * <p>
+     * This is the static method equivalent of applying the resulting function from
+     * {@link BinaryOperator#minBy(Comparator)} with the given comparator as argument.
+     *
+     * @return the least of the two given objects.
+     */
+    public static <T> T minBy(Comparator<? super T> propertyComparator, T t1, T t2) {
+        return propertyComparator.compare(t1, t2) > 0 ? t2 : t1;
+    }
+
+    /**
+     * Choose the lesser of two objects by comparing a
+     * {@link Comparable} value resolved from each object.
+     *
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * <p>
+     * This is the static method equivalent of applying the resulting function from
+     * {@link BinaryOperator#minBy(Comparator)} with
+     * {@link Comparator#comparing(Function, Comparator) comparing(propertyExtractor)}
+     * as argument.
+     *
+     * @return the least of the two given objects.
+     */
+    public static <T, U extends Comparable<? super U>> T minBy(Function<? super T, U> propertyExtractor, T t1, T t2) {
+        return minBy(comparing(propertyExtractor), t1, t2);
+    }
+
+
+    /**
+     * Choose the greater of two objects by using a {@link Comparator}.
+     *
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * <p>
+     * This is the static method equivalent of applying the resulting function from
+     * {@link BinaryOperator#maxBy(Comparator)} with the given comparator as argument.
+     *
+     * @return the least of the two given objects.
+     */
+    public static <T> T maxBy(Comparator<? super T> comparator, T t1, T t2) {
+        return comparator.compare(t2, t1) > 0 ? t2 : t1;
+    }
+
+    /**
+     * Choose the greater of two objects by comparing a
+     * {@link Comparable} value resolved from each object.
+     *
+     * In the case where the objects are considered equal by
+     * {@link Comparable#compareTo(Object)}, the first argument
+     * is returned.
+     *
+     * <p>
+     * This is the static method equivalent of applying the resulting function from
+     * {@link BinaryOperator#maxBy(Comparator)} with
+     * {@link Comparator#comparing(Function) comparing(propertyExtractor)}
+     * as argument.
+     *
+     * @return the least of the two given objects.
+     */
+    public static <T, U extends Comparable<? super U>> T maxBy(Function<? super T, U> propertyExtractor, T t1, T t2) {
+        return maxBy(comparing(propertyExtractor), t1, t2);
     }
 
     private DiggCompare() {}
--- src/test/java/no/digipost/DiggCompareTest.java
+++ src/test/java/no/digipost/DiggCompareTest.java
@@ -17,10 +17,15 @@ package no.digipost;
 
 import org.junit.jupiter.api.Test;
 import org.quicktheories.WithQuickTheories;
+import org.quicktheories.core.Gen;
 import org.quicktheories.dsl.TheoryBuilder2;
 
+import static java.util.Arrays.asList;
 import static no.digipost.DiggCompare.max;
+import static no.digipost.DiggCompare.maxBy;
 import static no.digipost.DiggCompare.min;
+import static no.digipost.DiggCompare.minBy;
+import static org.quicktheories.generators.Generate.pick;
 
 public class DiggCompareTest implements WithQuickTheories {
 
@@ -34,4 +39,21 @@ public class DiggCompareTest implements WithQuickTheories {
         forNonEqualsInts.check((x, y) -> min(x, y) == min(y, x));
         forNonEqualsInts.check((x, y) -> max(x, y) == max(y, x));
     }
+
+    enum Num {
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT
+    }
+
+    @Test
+    void minAndMaxByProperty() {
+        Gen<Num> anyNum = pick(asList(Num.values()));
+        TheoryBuilder2<Num, Num> forNonEqualsNums = qt()
+                .forAll(anyNum, anyNum)
+                .assuming((x, y) -> !x.equals(y));
+
+        forNonEqualsNums.check((x, y) -> maxBy(Num::ordinal, x, y) != minBy(Num::ordinal, x, y));
+        forNonEqualsNums.check((x, y) -> minBy(Num::ordinal, x, y) == minBy(Num::ordinal, y, x));
+        forNonEqualsNums.check((x, y) -> maxBy(Num::ordinal, x, y) == maxBy(Num::ordinal, y, x));
+    }
+
 }
